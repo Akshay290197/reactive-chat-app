11dd645d32f85e1ab1b7fa16654d9296124a2c89
Akshay Kumar <cakshay388@gmail.com>
2026-06-27 21:16:17 +0530

    chore: initial project setup

---
+++ src/main/java/com/chat/reactive/ReactiveChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/reactive/ReactiveChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
