b14029b6a21fdf2a6a530c5cd950dbbac6d0e835
Akshay Kumar <cakshay388@gmail.com>
2026-06-28 20:10:56 +0530

    authentication complete

--- src/main/java/com/chat/reactive/config/SecurityConfig.java
+++ src/main/java/com/chat/reactive/config/SecurityConfig.java
@@ -1,24 +1,45 @@
 package com.chat.reactive.config;
 
+import com.chat.reactive.security.JwtAuthenticationManager;
+import com.chat.reactive.security.JwtServerAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationManager jwtAuthenticationManager;
+    private final JwtServerAuthenticationConverter jwtServerAuthenticationConverter;
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .anyExchange().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults())
+                        .pathMatchers("/api/auth/**").permitAll()
+                        .anyExchange().authenticated())
+                .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationWebFilter authenticationWebFilter() {
+
+        AuthenticationWebFilter authenticationWebFilter =
+                new AuthenticationWebFilter(jwtAuthenticationManager);
+
+        authenticationWebFilter.setServerAuthenticationConverter(
+                jwtServerAuthenticationConverter
+        );
+
+        return authenticationWebFilter;
     }
 }
--- src/main/java/com/chat/reactive/security/JwtAuthenticationFilter.java
+++ src/main/java/com/chat/reactive/security/JwtAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.chat.reactive.security;
 
+import com.chat.reactive.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -10,8 +12,33 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter implements WebFilter {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        return chain.filter(exchange);
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return chain.filter(exchange);
+        }
+
+        String token = authHeader.substring(7);
+        String email = jwtService.extractUsername(token);
+
+        return userRepository.findByEmail(email)
+                .flatMap(user -> {
+                    if (!jwtService.isTokenValid(token, user)) {
+                        if (!jwtService.isTokenValid(token, user)) {
+                            return Mono.error(new BadCredentialsException("Invalid JWT Token"));
+                        }
+                    }
+
+                    return chain.filter(exchange);
+
+                })
+                .switchIfEmpty(chain.filter(exchange));
     }
 }
